1024624f2e751cbc1775559b1e67cb104cbc009d
Sven Eckelmann <sven@narfation.org>
2020-05-21 22:45:18 +0200

    wp-ffv-banner: Directly use dsgvo-youtube plugin
    The dsgvo-youtube can take care of generating the correct HTML code for the
    inner part of the banner. Only some styles have to be adjusted to make sure
    that the height is limited (while the content is centered) and the width of
    the image is the full with of the page.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- wp-ffv-banner.php
+++ wp-ffv-banner.php
@@ -8,17 +8,18 @@
  * Author URI: https://vogtland.freifunk.net
  */
 
+wp_register_style('wp-ffv-banner-style', plugins_url('css/wp-ffv-banner.css', __FILE__), [], false);
+wp_enqueue_style('wp-ffv-banner-style');
+
 add_action('vantage_after_masthead', function() {
 	if (!is_front_page())
 		return;
 
 ?>
 <div data-stretch="true">
-	<div style="position: relative; overflow: hidden; width: 100%; padding: 0; margin: 0 !important;">
-		<div style="position: relative; padding-bottom: 32.407407407407%; height: 0;">
-			<a data-fancybox="" href="esc_attr(//www.youtube.com/watch?v=2Z12OjnPADA)" style="position: absolute; top:-32.407407407407%; left:0; width: 100%; height: 100%">
-				<img style="display: block; height: auto; float: left; width: 100%; border: 0; vertical-align: middle;" src="/wordpress/wp-content/uploads/2020/05/2Z12OjnPADA.jpg">
-			</a>
+	<div class="ffv-banner">
+		<div class="ffv-banner-region">
+			<?= do_shortcode('[dsgvo-youtube url="//www.youtube.com/watch?v=2Z12OjnPADA" images="/wordpress/wp-content/uploads/2020/05/2Z12OjnPADA.jpg"][/dsgvo-youtube]'); ?>
 		</div>
 	</div>
 </div>
